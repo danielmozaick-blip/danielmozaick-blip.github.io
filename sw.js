1ca76219fdce863054a110d6a2293f4ce1ae9d80
danielmozaick-blip <danielmozaick@gmail.com>
2026-07-02 12:35:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'tala-menu-v2';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/boutique.html',
+  '/admin.html',
+  '/a-propos.html',
+  '/comment-ca-marche.html',
+  '/avis.html',
+  '/faq.html',
+  '/contact.html',
+  '/paiement.html',
+  '/style.css',
+  '/script.js',
+  '/config.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
